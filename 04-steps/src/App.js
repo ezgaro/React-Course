4a67f307246a29b4568c3ca272d3d5570afb93ea
ezgaro <ezgaro2005@gmail.com>
2024-08-24 15:40:34 +0300

    adding close utton functionality

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -9,6 +9,7 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
@@ -17,25 +18,37 @@ export default function App() {
     if (step < 3) setStep(step + 1);
   }
 
+  function handleClose() {
+    if(isOpen) {
+      setIsOpen(false);
+    } else {
+      setIsOpen(true);
+    }
+  }
+
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          onClick={handlePrevious}
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-        >
-          Previous
-        </button>
-        <button onClick={handleNext}>Next</button>
-      </div>
-    </div>
+    <>
+      <button onClick={handleClose} className="close">&times;</button>
+
+      {isOpen && (<div className="steps">
+        <div className="numbers">
+          <div className={step >= 1 ? "active" : ""}>1</div>
+          <div className={step >= 2 ? "active" : ""}>2</div>
+          <div className={step >= 3 ? "active" : ""}>3</div>
+        </div>
+        <p className="message">
+          Step {step}: {messages[step - 1]}
+        </p>
+        <div className="buttons">
+          <button
+            onClick={handlePrevious}
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          >
+            Previous
+          </button>
+          <button onClick={handleNext}>Next</button>
+        </div>
+      </div>)}
+    </>
   );
 }
