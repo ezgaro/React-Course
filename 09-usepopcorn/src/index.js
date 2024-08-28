38e405f26f797f25bbe3586ec87085c874399f8f
ezgaro <ezgaro2005@gmail.com>
2024-08-28 14:05:36 +0300

    Adding the new component for rating

--- 09-usepopcorn/src/index.js
+++ 09-usepopcorn/src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing "]}
+      defaultRating={3}
+    />
+    <StarRating size={24} color="red" maxRating={10} className="test"/>
   </React.StrictMode>
 );
