7b99eceb6ea4f584984ccee882a30697fc2e51b7
ezgaro <ezgaro2005@gmail.com>
2024-09-01 19:21:05 +0300

    Adding lyfecycle methods to the application

--- 11-classy-weather/src/App.js
+++ 11-classy-weather/src/App.js
@@ -60,7 +60,7 @@ async function getWeather(location) {
 
 class App extends React.Component {
   state = {
-    location: "varna",
+    location: "",
     isLoading: false,
     displayLocation: "",
     weather: {},
@@ -68,6 +68,7 @@ class App extends React.Component {
 
   // async fetchWeather() {
   fetchWeather = async () => {
+    if (this.state.location.length < 2) return this.setState({ weather: {} });
     try {
       this.setState({ isLoading: true });
       // 1) Getting location (geocoding)
@@ -92,13 +93,27 @@ class App extends React.Component {
       const weatherData = await weatherRes.json();
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.err(err);
+      console.error(err);
     } finally {
       this.setState({ isLoading: false });
     }
   };
 
   setLocation = (e) => this.setState({ location: e.target.value });
+
+  //useEffect []
+  componentDidMount() {
+    // this.fetchWeather();
+    this.setState({ location: localStorage.getItem("location") || "" });
+  }
+
+  //useEffect [some variable]
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+      localStorage.setItem("location", this.state.location);
+    }
+  }
 
   render() {
     return (
@@ -108,9 +123,6 @@ class App extends React.Component {
           location={this.state.location}
           onChangeLocation={this.setLocation}
         />
-        <button onClick={this.fetchWeather}>
-          Get weather
-        </button>
         {this.state.isLoading && <p className="loader">Loading...</p>}
 
         {this.state.weather.weathercode && (
@@ -127,6 +139,10 @@ class App extends React.Component {
 export default App;
 
 class Weather extends React.Component {
+  componentWillUnmount() {
+    console.log("Weather is unmounting !");
+  }
+
   render() {
     const {
       temperature_2m_max: max,
