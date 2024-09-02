c37888b293edd78a2d5cd6868a83f5846908333f
ezgaro <ezgaro2005@gmail.com>
2024-09-02 11:30:14 +0300

    Starting new quiz funcitonality

--- 12-react-quiz/src/App.js
+++ 12-react-quiz/src/App.js
@@ -1,13 +1,54 @@
-import Header from './Header';
-import Main from './Main';
+import { useEffect } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import { useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  //'loading','error', 'ready', 'active', 'finished'
+  status: "loading",
+  index: 0
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    default:
+      throw new Error("Action is unknown");
+  }
+}
 
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((error) => dispatch({ status: "error" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <Main className='main'>
-        <p>1/15</p>
-        <p>Question?</p>
+      <Main className="main">
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen dispatch={dispatch} numQuestions={numQuestions} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
