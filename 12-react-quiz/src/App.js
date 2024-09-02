1a9a3fa3eb6d4c7ab64f692a3d6f9486f2ba8d11
ezgaro <ezgaro2005@gmail.com>
2024-09-02 10:41:08 +0300

    Adding the Main component

--- 12-react-quiz/src/App.js
+++ 12-react-quiz/src/App.js
@@ -1,9 +1,14 @@
-import DateCounter from "./DateCounter";
+import Header from './Header';
+import Main from './Main';
 
 export default function App() {
   return (
-    <h1>
-      <DateCounter />
-    </h1>
+    <div className="app">
+      <Header />
+      <Main className='main'>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
   );
 }
