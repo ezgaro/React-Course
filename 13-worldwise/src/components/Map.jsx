39a8a11a3e560e00c1118043feb2bdede481f5b2
ezgaro <ezgaro2005@gmail.com>
2024-09-05 13:06:51 +0300

    interacting with the map

--- 13-worldwise/src/components/Map.jsx
+++ 13-worldwise/src/components/Map.jsx
@@ -1,22 +1,34 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvent,
+  useMapEvents,
+} from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 
 export default function Map() {
-  const navigate = useNavigate();
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  useEffect(() => {
+    if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+  }, [mapLat, mapLng]);
   return (
     <div className={styles.mapContainer}>
       <MapContainer
+        // center={[mapLat, mapLng]}
         center={mapPosition}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -35,7 +47,25 @@ export default function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChageCenter position={mapPosition} />
+        <DetactClick />
       </MapContainer>
     </div>
   );
+}
+
+function ChageCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function DetactClick() {
+  const navigate = useNavigate();
+
+  useMapEvents({
+    click: (e) => {
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
 }
