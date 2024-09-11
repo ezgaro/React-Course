68328be67fc18b1dbc26d487d612efffc320e9c5
ezgaro <ezgaro2005@gmail.com>
2024-09-11 15:51:32 +0300

    setting up tailwinds

---
+++ 17-fast-react-pizza/prettier.config.cjs
@@ -0,0 +1,7 @@
+import prettierPluginTailwindcss from "prettier-plugin-tailwindcss";
+
+export default {
+  plugins: [prettierPluginTailwindcss],
+  singleQuote: true,
+  // Add any other Prettier options you need here
+};
