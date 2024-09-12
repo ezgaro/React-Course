ddafb3633cdd5893ba5cf00f0c26fdf0a47551ff
ezgaro <ezgaro2005@gmail.com>
2024-09-12 11:07:58 +0300

    Reusing Styles With React Components

--- 17-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 17-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,5 +1,6 @@
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom'
 import { createOrder } from '../../services/apiRestaurant'
+import Button from '../../ui/Button'
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -62,7 +63,7 @@ function CreateOrder() {
                     <label>Address</label>
                     <div>
                         <input
-                            className="w-full rounded-full border border-stone-200 px-4 py-2 md:py-3 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6"
+                            className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
                             type="text"
                             name="address"
                             required
@@ -72,7 +73,7 @@ function CreateOrder() {
 
                 <div>
                     <input
-                    className='h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2'
+                        className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
                         type="checkbox"
                         name="priority"
                         id="priority"
@@ -90,12 +91,9 @@ function CreateOrder() {
                         name="cart"
                         value={JSON.stringify(cart)}
                     />
-                    <button
-                        disabled={isSubmiting}
-                        className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
-                    >
+                    <Button>
                         {isSubmiting ? 'Placing order...' : 'Order now'}
-                    </button>
+                    </Button>
                 </div>
             </Form>
         </div>
