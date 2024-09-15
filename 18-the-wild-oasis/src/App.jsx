394f262346f01a14cdae49b4904f5d9c64a9091e
ezgaro <ezgaro2005@gmail.com>
2024-09-15 13:24:35 +0300

    Adding starter files for the Wild Oasis application

---
+++ 18-the-wild-oasis/src/App.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Clicked")}>Check In</Button>
+        <Button onClick={() => alert("Clicked")}>Check Out</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
+}
