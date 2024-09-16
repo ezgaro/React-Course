347784f1343d3e182c3d1a3b22b059be9dfa5d6a
ezgaro <ezgaro2005@gmail.com>
2024-09-16 10:56:40 +0300

    Connecting Supabase With Our React App

---
+++ 18-the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://olbrdbmqzolzsaryikci.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sYnJkYm1xem9senNhcnlpa2NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY0Njk2NjEsImV4cCI6MjA0MjA0NTY2MX0.uJ5nkb1ntbTVrjnDUIf9Do41qmdvILnyxd578D6sP6s";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
