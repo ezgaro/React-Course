f970b4368167879df2bea90230354359a5c9981b
ezgaro <ezgaro2005@gmail.com>
2024-09-21 11:36:49 +0300

    ClientSide Sorting Sorting Cabins

--- 18-the-wild-oasis/src/ui/Select.jsx
+++ 18-the-wild-oasis/src/ui/Select.jsx
@@ -14,9 +14,9 @@ const StyledSelect = styled.select`
   box-shadow: var(--shadow-sm);
 `;
 
-export default function Select({ options, value }) {
+export default function Select({ options, value, onChange, ...props }) {
   return (
-    <StyledSelect value={value}>
+    <StyledSelect value={value} onChange={onChange} {...props}>
       {options.map((option) => (
         <option key={option.value} value={option.value}>
           {option.label}
