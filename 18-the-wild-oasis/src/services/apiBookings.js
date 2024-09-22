62c020f5626f188d85ddbef60d4dcaef6d094e8a
ezgaro <ezgaro2005@gmail.com>
2024-09-22 09:23:03 +0300

    APISide Sorting Sorting Bookings

--- 18-the-wild-oasis/src/services/apiBookings.js
+++ 18-the-wild-oasis/src/services/apiBookings.js
@@ -8,9 +8,14 @@ export async function getBookings({ filter, sortBy }) {
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
     );
   //FLTER
-  if (filter !== null)
-    query = query[filter.method || "eq"](filter.field, filter.value);
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
+
+  //SORTING
+  if (sortBy) {
+    query = query.order(sortBy.field, {ascending: sortBy.direction === 'asc'});
+  }
   const { data, error } = await query;
+
   if (error) {
     console.error(error);
     throw new Error("Bookings could not be created");
