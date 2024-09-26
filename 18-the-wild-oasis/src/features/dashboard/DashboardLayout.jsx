165e6329e8d4e4a4bf0c9eb051a73afb55184637
ezgaro <ezgaro2005@gmail.com>
2024-09-26 15:17:13 +0300

    Displaying a Line Chart With the Recharts Library

--- 18-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 18-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import Spinner from "../../ui/Spinner";
 import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -33,7 +34,7 @@ export default function DashboardLayout() {
         cabinCount={cabins.length}
       />
       <div>Today's activity</div>
-      <div>Chart stay durations</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
