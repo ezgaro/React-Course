967763d60e1e1a7b7cd77670f54b9f7f5b782cc4
ezgaro <ezgaro2005@gmail.com>
2024-09-26 07:39:02 +0300

    updating user fullName

--- 18-the-wild-oasis/src/features/authentication/UpdateUserDataForm.jsx
+++ 18-the-wild-oasis/src/features/authentication/UpdateUserDataForm.jsx
@@ -7,6 +7,7 @@ import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
 import { useUser } from "./useUser";
+import { useUpdateUser } from "./useUpdateUser";
 
 function UpdateUserDataForm() {
   // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
@@ -17,11 +18,15 @@ function UpdateUserDataForm() {
     },
   } = useUser();
 
+  const { updateUser, isUpdating } = useUpdateUser();
+
   const [fullName, setFullName] = useState(currentFullName);
   const [avatar, setAvatar] = useState(null);
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!fullName) return;
+    updateUser({ fullName, avatar });
   }
 
   return (
@@ -31,24 +36,29 @@ function UpdateUserDataForm() {
       </FormRow>
       <FormRow label="Full name">
         <Input
+          disabled={isUpdating}
           type="text"
           value={fullName}
-          onChange={(e) => setFullName(e.target.value)}
+          onChange={(e) => {
+            console.log(e.target.value);
+            return setFullName(e.target.value);
+          }}
           id="fullName"
         />
       </FormRow>
       <FormRow label="Avatar image">
         <FileInput
+          disabled={isUpdating}
           id="avatar"
           accept="image/*"
           onChange={(e) => setAvatar(e.target.files[0])}
         />
       </FormRow>
       <FormRow>
-        <Button type="reset" variation="secondary">
+        <Button disabled={isUpdating} type="reset" variation="secondary">
           Cancel
         </Button>
-        <Button>Update account</Button>
+        <Button disabled={isUpdating}>Update account</Button>
       </FormRow>
     </Form>
   );
